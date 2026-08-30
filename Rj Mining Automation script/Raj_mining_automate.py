4ff495065aa1a5740916689e2101714107187388
DhruvBhardwaj734 <dhruvbhardwaj734@gmail.com>
2026-08-30 23:32:28 +0530

    Rajasthan automation  Vendor id is working

--- Rj Mining Automation script/Raj_mining_automate.py
+++ Rj Mining Automation script/Raj_mining_automate.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import expect
 import openpyxl as xl
+import re
 workbook = xl.load_workbook("RAJ_MINING_DEVICES.xlsx")
 sheet = workbook["Sheet1"]
 Devices=[]
@@ -7,15 +9,7 @@ for row in sheet.iter_rows(min_row=2, values_only=True):
     imei , vehicle_no ,_= row
     Devices.append((imei,vehicle_no))
 print(Devices)
-
-
-
-
 input("entererr")
-
-
-
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
@@ -42,6 +36,11 @@ with sync_playwright() as p:
     Reg_device.click()
     # imei = input("Enter IMEI number:")
     # search_imei = page.locator("input[placeholder='IMEI/Serial || Vehicle Regd.No']")
+    # def select_when_ready(select_locator, label, timeout=10000):
+                    # option = select_locator.locator("option").filter(
+                    # has_text=re.compile(f"^{re.escape(label.strip())}$"))
+                    # expect(option).to_be_attached(timeout=timeout)
+                    # select_locator.select_option(label=label)
     for imei, vehicle_no in Devices:          
         imei = str(imei)                     
         vehicle_no = str(vehicle_no)
@@ -59,45 +58,68 @@ with sync_playwright() as p:
             # add_devc(imei)
             add_dvc = page.get_by_role("button", name="Add Device")
             add_dvc.click()
+            wait()
             vendor = page.locator('select[formcontrolname="vendorId"]')
-            vendor.select_option(label="WATSOO EXPRESS PRIVATE LIMITED")
+            # # vendor.select_option(value="WATSOO EXPRESS PRIVATE LIMITED")
+            # vendor.select_option(index=1)
+            # input("Vendor id request is selcted")
+            vendor.select_option(value="224")
+            print("Raw DOM value after select_option:", vendor.input_value())
+            page.wait_for_timeout(500)
+            print("Raw DOM value after short wait:", vendor.input_value())
+            vendor.select_option(value="224")
+            vendor.dispatch_event("change")
+            vendor.dispatch_event("input")
+            page.wait_for_timeout(300)
+            print("Value after manual dispatch:", vendor.input_value())
+
+            
+            # add_dvc.click()
+            # vendor = page.locator('select[formcontrolname="vendorId"]')
+            # select_when_ready(vendor, "WATSOO EXPRESS PRIVATE LIMITED")
+            # input("Vendor is selected")
+            # options_now = vendor.locator("option").all_text_contents()
+            # print(f"[{imei}] Vendor options before selecting:", options_now)
+            # select_when_ready(vendor, "WATSOO EXPRESS PRIVATE LIMITED")
 
             test_req = page.locator('select[formcontrolname="requestNo"]')
             test_req.select_option(label="REQVEN-1-305")
+            input("Test request is selcted")
 
             manuf_name = page.locator('select[formcontrolname="manufacturerName"]')
             manuf_name.select_option(label="WATSOO EXPRESS PRIVATE LIMITED")
+            input("Manufacturer name is selected")
             Enter_imei = page.get_by_role("textbox", name="Enter IMEI or Serial Number")
             Enter_imei.fill(imei)
             # Vin = input("Enter Vehicle Number:")
             Veh_no = page.get_by_placeholder("Enter vehicle registration")
             Veh_no.fill(vehicle_no)
             Verify = page.get_by_role("button", name="Verify").click()
-            wait()
+            page.wait_for_timeout(5000)
             already_exit = page.get_by_text("Vehicle number already exist.")
-            if already_exit.is_visible():
-                print("Vehicle number already exist")
-                close= page.get_by_title("Close")
-                close.click()
-            else:
-                Date = page.locator('[formcontrolname="installDateTime"]').click()
-                Wait_state = page.locator('.flatpickr-calendar.open').wait_for(state="visible")
-                today_date = page.locator('.flatpickr-calendar.open .flatpickr-day.today').click()
-                # Date.click()
-                image = page.locator("input[type='file']").set_input_files(r"C:\Users\Admin\Desktop\Playwright Leaning\4G Device pic.png")
-                Sme_zone = page.locator('select[formcontrolname="smeOfficeName"]')
-                Sme_zone.select_option(label="AJMER")
-                Me_district = page.locator('select[formcontrolname="meOfficeName"]')
-                Me_district.select_option(label="AME GOTAN")
-                page.wait_for_load_state("networkidle")
-                checkbox = page.locator('[formcontrolname="healthDeclarationAccepted"]').check()
-                Register_device = page.get_by_role("button", name="Register Device")
-                Register_device.click()
-                ok()
-                device_tagging.click()
-                Reg_device.click()
+            # if already_exit.is_visible():
+            # print("Vehicle number already exist")
+            # close= page.get_by_title("Close")
+            # close.click()
+            # else:
+            Date = page.locator('[formcontrolname="installDateTime"]').click()
+            Wait_state = page.locator('.flatpickr-calendar.open').wait_for(state="visible")
+            today_date = page.locator('.flatpickr-calendar.open .flatpickr-day.today').click()
+            # Date.click()
+            image = page.locator("input[type='file']").set_input_files(r"C:\Users\Admin\Desktop\Playwright Leaning\4G Device pic.png")
+            Sme_zone = page.locator('select[formcontrolname="smeOfficeName"]')
+            Sme_zone.select_option(label="AJMER")
+            Me_district = page.locator('select[formcontrolname="meOfficeName"]')
+            Me_district.select_option(label="AME GOTAN")
+            page.wait_for_load_state("networkidle")
+            checkbox = page.locator('[formcontrolname="healthDeclarationAccepted"]').check()
+            Register_device = page.get_by_role("button", name="Register Device")
+            Register_device.click()
+            ok()
             device_tagging.click()
             Reg_device.click()
+            # device_tagging.click()
+            # Reg_device.click()
         
         else:
             print("imei is already registered")
--- Vhn_tagging.py
+++ Vhn_tagging.py
@@ -10,8 +10,26 @@ with sync_playwright() as p:
     pass_fill.fill("Watsoo@2024")
     input("Enter after login ")
     lgn = page.get_by_role("button", name="Login").click()
-    Tag = page.get_by_role("link", name="Add/Remove VLTD").click()
-    checkbox = page.get_by_role('radio', name='rb').click()
+    Tag = page.get_by_text("Tag/Untag VLTD").click()
+    add_remove = page.get_by_text("Add/Remove VLTD")
+    add_remove.wait_for(state="visible", timeout=5000)
+    add_remove.click()
+    # checkbox = page.get_by_role('radio', name='rb').check()
+    radio_wrapper = page.locator('div.ui-radiobutton').filter(
+    has=page.locator('input[value="A"]')) 
+    radio_wrapper.locator('.ui-radiobutton-box').click()
+    input("wait")
+
+    select_vltd_maker = page.get_by_text("Select VLTD Maker").click()
+    wat_exp = page.get_by_role('option',name="WATSOO EXPRESS PVT LTD").click()
+    input("wait")
+    select_vltd_model = page.get_by_text("Select VLTD Model").click()
+    model= input("Enter 2g or 4g").upper()
+    if (model=="2G"):
+        Model_2g= page.get_by_role('option',name="PRITHIVI 140").click()
+    else:
+        Model_4g= page.get_by_role('option',name="PRITHVI 140 4G").click()
+
     input("enter to close")
     # search = page.get_by_role("button", name=)
     browser.close()
